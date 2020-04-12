4a31b2dbd1a16d0e23ab1471a4a6e2d5f3addddd
devang <devangnpatil@gmail.com>
2020-04-12 14:13:18 +0530

    render dom once firebase auth is ready

--- src/index.js
+++ src/index.js
@@ -15,21 +15,25 @@ const store = createStore(
   rootReducer, 
   compose(
     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-    reactReduxFirebase(fbConfig),
+    reactReduxFirebase(fbConfig, {attachAuthIsReady: true}),
     reduxFirestore(fbConfig)
   )
   );
 
-ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-      </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
-);
+store.firebaseAuthIsReady.then(()=> {
+  ReactDOM.render(
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+        </React.StrictMode>
+    </Provider>,
+    document.getElementById('root')
+  );
+  
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://bit.ly/CRA-PWA
+  serviceWorker.unregister();
+})
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
