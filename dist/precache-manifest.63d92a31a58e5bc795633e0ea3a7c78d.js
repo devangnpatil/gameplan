f03be6bba2091c4d1ee3bac704dda3aba615d757
devang <devangnpatil@gmail.com>
2020-04-12 20:42:34 +0530

    push to dust dir

---
+++ dist/precache-manifest.63d92a31a58e5bc795633e0ea3a7c78d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef8206b201341594dc91ff68eb96a47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a27d67b64b3eddd36584",
+    "url": "/static/css/main.4fac6c57.chunk.css"
+  },
+  {
+    "revision": "b30e3423fc4edc51ed3b",
+    "url": "/static/js/2.61e84a9f.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/static/js/2.61e84a9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a27d67b64b3eddd36584",
+    "url": "/static/js/main.ea00b2d6.chunk.js"
+  },
+  {
+    "revision": "0eb42e0cf4e3622b9fa9",
+    "url": "/static/js/runtime-main.eafb9be1.js"
+  }
+]);
