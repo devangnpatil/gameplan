c7e3b037279c1341edcd024b6b5445d4cdd83ded
devang <devangnpatil@gmail.com>
2020-04-12 13:34:27 +0530

    handle sign in link

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,22 +3,23 @@ import { Link } from 'react-router-dom';
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
 import {connect} from 'react-redux';
-const Navbar = () => {
+const Navbar = (props) => {
+    const { auth } = props;
+    // console.log('auth', auth)
+    const links = auth.uid ? <SignedInLinks/> : <SignedOutLinks />
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to='/' className="brand-logo">Game</Link>
-                <SignedInLinks></SignedInLinks>
-                <SignedOutLinks></SignedOutLinks>
+                {links}
             </div>
         </nav>
     )
 }
 
 const mapStateToProps = (state) => {
-    console.log('Login', state)
     return {
-
+        auth: state.firebase.auth
     }
 }
 
