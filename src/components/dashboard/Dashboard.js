ef24d1e3b1ffa903e363d7c0a00a1bb1227444e4
devang <devangnpatil@gmail.com>
2020-04-12 20:24:29 +0530

    add notification to frontend

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -7,7 +7,7 @@ import { firestoreConnect } from 'react-redux-firebase'
 import {Redirect} from 'react-router-dom'
 export class Dashboard extends Component {
     render() {
-        const {project, auth} = this.props;
+        const {project, auth, notifications} = this.props;
         
         if(!auth.uid) return <Redirect to='/signin'/>
         return (
@@ -17,7 +17,7 @@ export class Dashboard extends Component {
                         <ProjectList projects={project}/>
                     </div>
                     <div className="col s12 m5 offset-m1">
-                        <Notifications />
+                        <Notifications notifications={notifications}/>
                     </div>
                 </div>
             </div>
@@ -28,13 +28,15 @@ export class Dashboard extends Component {
 const mapStateToProps = (state) => {
     return {
         project: state.firestore.ordered.projects,
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        notifications: state.firestore.ordered.notifications
     }
 }
 
 export default compose(
     connect(mapStateToProps),
     firestoreConnect([
-        { collection: 'projects' }
+        { collection: 'projects' },
+        { collection: 'notifications', limit: 3 }
     ])
 )(Dashboard)
